ef96b7d3ec507097a8eb8e7f07f30e5089ef8aff
Ramon Barros <ramondesbarros@gmail.com>
2022-09-07 03:31:06 -0300

    atualizacao da query

--- src/main/resources/schema-all.sql
+++ src/main/resources/schema-all.sql
@@ -1,14 +1,7 @@
 DROP TABLE people IF EXISTS;
 
 CREATE TABLE people (
-    id SMALLINT PRIMARY KEY,
-    firstname VARCHAR(20),
-    lastname VARCHAR(20),
-    city VARCHAR(20),
-    age VARCHAR(3)
-);
-
-CREATE TABLE people (
+    id SMALLINT PRIMARY KEY AUTO_INCREMENT,	
     firstname VARCHAR(20),
     lastname VARCHAR(20),
     city VARCHAR(20),
--- target/classes/schema-all.sql
+++ target/classes/schema-all.sql
@@ -8,7 +8,26 @@ CREATE TABLE people (
     age VARCHAR(3)
 );
 
+CREATE TABLE person (
+    id SMALLINT PRIMARY KEY AUTO_INCREMENT,
+    firstname VARCHAR(20),
+    lastname VARCHAR(20),
+    city VARCHAR(20),
+    age VARCHAR(3)
+);
+
+
 CREATE TABLE people (
+    id SMALLINT PRIMARY KEY AUTO_INCREMENT,	
+    firstname VARCHAR(20),
+    lastname VARCHAR(20),
+    city VARCHAR(20),
+    age VARCHAR(3)
+);
+
+
+CREATE TABLE person (
+    id SMALLINT PRIMARY KEY AUTO_INCREMENT,
     firstname VARCHAR(20),
     lastname VARCHAR(20),
     city VARCHAR(20),
